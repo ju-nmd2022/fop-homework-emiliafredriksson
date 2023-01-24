029bcaf5202528ff946ce92447a1f852978466d8
Emilia Fredriksson <frem22pu@student.ju.se>
2023-01-24 11:06:12 +0100

    color

--- drawing-program.js
+++ drawing-program.js
@@ -4,7 +4,8 @@ let lastY = 0;
 background(100);
 
 function draw() {
-  stroke(mouseX, 0, mouseY);
+  strokeWeight(5);
+  stroke((mouseX / width) * 255, 0, (mouseY / height) * 255);
   line(lastX, lastY, mouseX, mouseY);
 
   lastX = mouseX;
